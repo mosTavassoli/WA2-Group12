8d24bfb1bfd73fb8be7dd90f5fee0304db41f93d
ciaosonocampa <mcampanini44@gmail.com>
2021-04-09 17:50:54 +0200

    POST /wallet initialized

--- src/main/kotlin/com/wa2/demo/controllers/WalletController.kt
+++ src/main/kotlin/com/wa2/demo/controllers/WalletController.kt
@@ -3,11 +3,31 @@ package com.wa2.demo.controllers
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 
 @Controller
 class WalletController {
+
+    @PostMapping("/wallet")
+    fun createWallet(){
+
+        //TODO create Wallet
+        println("Creating wallet...")
+
+
+    }
+
+
+
     @GetMapping("/wallet/{walletId}/transaction")
     fun createTransaction(@PathVariable walletId: Long) {
         //TODO
+        println("Got the message!")
     }
+
+
+
+
+
+
 }
