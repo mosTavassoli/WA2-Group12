809e5b061ec18adfc08c54eb18f03ff8eab626a6
mosTavassoli <m.tavassoli.it@gmail.com>
2021-04-20 12:55:34 +0200

    fix: disable spring security

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,10 +36,10 @@ dependencies {
     implementation ("javax.validation:validation-api")
     implementation ("com.google.code.gson:gson:2.8.5")
     //------- JWT Dependencies -------
-    implementation("io.jsonwebtoken:jjwt-api:0.11.2")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
-    implementation("org.springframework.boot:spring-boot-starter-security")
+//    implementation("io.jsonwebtoken:jjwt-api:0.11.2")
+//    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
+//    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
+//    implementation("org.springframework.boot:spring-boot-starter-security")
     //------- END JWT Dependencies -------
 }
 
--- src/main/kotlin/com/wa2/demo/DemoApplication.kt
+++ src/main/kotlin/com/wa2/demo/DemoApplication.kt
@@ -2,16 +2,14 @@ package com.wa2.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.security.crypto.factory.PasswordEncoderFactories
-import org.springframework.security.crypto.password.PasswordEncoder
+
 
 @SpringBootApplication
 class DemoApplication{
-    @Bean
-    fun passwordEncoder() : PasswordEncoder {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder()
-    }
+//    @Bean
+//    fun passwordEncoder() : PasswordEncoder {
+//        return PasswordEncoderFactories.createDelegatingPasswordEncoder()
+//    }
 }
 
 
