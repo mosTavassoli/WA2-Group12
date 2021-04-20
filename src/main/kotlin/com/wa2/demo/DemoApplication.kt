15dd62f3e6633552dd0cf285a03c86be282b59c4
mosTavassoli <m.tavassoli.it@gmail.com>
2021-04-20 12:41:04 +0200

    feat: merge to master

--- src/main/kotlin/com/wa2/demo/DemoApplication.kt
+++ src/main/kotlin/com/wa2/demo/DemoApplication.kt
@@ -2,9 +2,18 @@ package com.wa2.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @SpringBootApplication
-class DemoApplication
+class DemoApplication{
+    @Bean
+    fun passwordEncoder() : PasswordEncoder {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder()
+    }
+}
+
 
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
---
+++ src/main/kotlin/com/wa2/demo/security/JwtUtils.kt
@@ -0,0 +1,27 @@
+package com.wa2.demo.security
+
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.core.Authentication
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class JwtUtils {
+
+//    @Value("\${application.jwt.jwtSecret}")
+//    lateinit var secretKey : String
+//
+//    @Value("\${application.jwt.jwtExpirationMs}")
+//    lateinit var expirationTime : String
+//
+//    fun generateJwtToken (authentication: Authentication): String{
+//        // we need a user ID
+////        val issuer = UserDetailsDTO.id.toString()
+//        val jwt = Jwts.builder()
+//            .setIssuer(issuer)
+//            .setExpiration(Date(System.currentTimeMillis() + expirationTime.toLong()))
+//            .signWith()
+//    }
+}
